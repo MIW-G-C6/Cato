47aee45fd4729f12b094053c81c72c9ae1e395f7
Remco Lantinga <remco_lantinga@hotmail.com>
2021-10-05 12:01:41 +0200

    created spring boot projects

---
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/CaToApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se6.heartcoded.CaTo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaToApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/se6/heartcoded/CaTo/CaToApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se6.heartcoded.CaTo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
