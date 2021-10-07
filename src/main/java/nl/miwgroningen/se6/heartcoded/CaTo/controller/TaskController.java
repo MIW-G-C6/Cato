1b3950e9d85af5a234f6a6dacecf9e37f5c64656
Remco Lantinga <remco_lantinga@hotmail.com>
2021-10-07 10:56:02 +0200

    site admin can now view tasks in tasklists and add tasks to task list

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/TaskController.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/TaskController.java
@@ -1,6 +1,18 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.controller;
 
+import nl.miwgroningen.se6.heartcoded.CaTo.model.Task;
+import nl.miwgroningen.se6.heartcoded.CaTo.model.TaskList;
+import nl.miwgroningen.se6.heartcoded.CaTo.repository.TaskListRepository;
+import nl.miwgroningen.se6.heartcoded.CaTo.repository.TaskRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 /**
  * @author Erwin Wegter <ewegter@gmail.com>
@@ -10,4 +22,34 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TaskController {
 
+    TaskRepository taskRepository;
+    TaskListRepository taskListRepository;
+
+    public TaskController(TaskRepository taskRepository, TaskListRepository taskListRepository) {
+        this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
+    }
+
+    @GetMapping("/taskLists/{taskListId}/new")
+    protected String showTaskForm(@PathVariable("taskListId") Integer taskListId, Model model) {
+        Optional<TaskList> taskList = taskListRepository.findById(taskListId);
+        if (taskList.isEmpty()) {
+            return "redirect:/taskLists";
+        }
+        model.addAttribute("task", new Task());
+        model.addAttribute("taskList", taskListRepository.getById(taskListId));
+        return "taskForm";
+    }
+
+    @PostMapping("/taskLists/{taskListId}/new")
+    protected String saveOrUpdateTask(
+            @PathVariable ("taskListId") Integer taskListId,
+            @ModelAttribute("task") Task task, BindingResult result) {
+
+        if (!result.hasErrors()) {
+            task.setTaskList(taskListRepository.getById(taskListId));
+            taskRepository.save(task);
+        }
+        return "redirect:/taskLists/{taskListId}";
+    }
 }
--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/TaskListController.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/TaskListController.java
@@ -1,10 +1,16 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.controller;
 
+import nl.miwgroningen.se6.heartcoded.CaTo.model.TaskList;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.TaskListRepository;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.TaskRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Optional;
+
 /**
  * @author Erwin Wegter <ewegter@gmail.com>
  *
@@ -23,8 +29,18 @@ public class TaskListController {
     }
 
     @GetMapping("/taskLists")
-    public String showTaskListOverview(Model model) {
+    protected String showTaskListOverview(Model model) {
         model.addAttribute("allTaskList", taskListRepository.findAll());
         return "taskListOverview";
+    }
+
+    @GetMapping("/taskLists/{taskListId}")
+    protected String showTaskListDetails(@PathVariable("taskListId") Integer taskListId, Model model) {
+        Optional<TaskList> taskList = taskListRepository.findById(taskListId);
+        if (taskList.isEmpty()) {
+            return "redirect:/taskLists";
+        }
+        model.addAttribute("taskList", taskList.get());
+        return "taskListDetails";
     }
 }
