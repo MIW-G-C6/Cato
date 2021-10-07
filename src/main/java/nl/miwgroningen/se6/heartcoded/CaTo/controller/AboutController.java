53ae729707400d5675713485ee1c1b6dded79ca1
Remco Lantinga <remco_lantinga@hotmail.com>
2021-10-07 15:05:04 +0200

    about page added

---
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/AboutController.java
@@ -0,0 +1,20 @@
+package nl.miwgroningen.se6.heartcoded.CaTo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Remco Lantinga <remco_lantinga@hotmail.com>
+ * Controls the about page.
+ */
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    protected String showAboutPage() {
+        return "aboutPage";
+    }
+
+
+}
