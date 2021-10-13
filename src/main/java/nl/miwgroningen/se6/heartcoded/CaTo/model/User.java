540172d3da45f9585d2c81cdffa606f00db0ec29
Shalena Omapersad <shalenao@hotmail.com>
2021-10-13 10:14:29 +0200

    Security implemented. A user can now login

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/model/User.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/model/User.java
@@ -1,9 +1,16 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Shalena Omapersad <shalenao@hotmail.com>
@@ -12,7 +19,7 @@ import javax.persistence.Id;
  */
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -22,7 +29,7 @@ public class User {
 
     private String password;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String email;
 
     public User(Integer userId, String name, String email) {
@@ -32,6 +39,46 @@ public class User {
     }
 
     public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return authorityList;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Integer getUserId() {
@@ -56,13 +103,5 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
