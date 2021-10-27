61e611a899d84c9207afcc797fd4c8b383fc8814
Erwin Wegter <ewegter@gmail.com>
2021-10-27 15:13:07 +0200

    Site admin now has an overview page with all user, groups and clients

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/service/GroupHasUsersService.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/service/GroupHasUsersService.java
@@ -2,9 +2,11 @@ package nl.miwgroningen.se6.heartcoded.CaTo.service;
 
 import nl.miwgroningen.se6.heartcoded.CaTo.model.Group;
 import nl.miwgroningen.se6.heartcoded.CaTo.model.GroupHasUsers;
+import nl.miwgroningen.se6.heartcoded.CaTo.model.TaskList;
 import nl.miwgroningen.se6.heartcoded.CaTo.model.User;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.GroupHasUsersRepository;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.GroupRepository;
+import nl.miwgroningen.se6.heartcoded.CaTo.repository.TaskListRepository;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,13 +27,15 @@ public class GroupHasUsersService {
     GroupRepository groupRepository;
     UserRepository userRepository;
     GroupHasUsersRepository groupHasUsersRepository;
+    TaskListRepository taskListRepository;
 
-    public GroupHasUsersService(GroupRepository groupRepository,
-                                UserRepository userRepository,
-                                GroupHasUsersRepository groupHasUsersRepository) {
+    public GroupHasUsersService(GroupRepository groupRepository, UserRepository userRepository,
+                                GroupHasUsersRepository groupHasUsersRepository,
+                                TaskListRepository taskListRepository) {
         this.groupRepository = groupRepository;
         this.userRepository = userRepository;
         this.groupHasUsersRepository = groupHasUsersRepository;
+        this.taskListRepository = taskListRepository;
     }
 
     public void saveGroupHasUsers(GroupHasUsers groupHasUsers) {
@@ -59,5 +63,15 @@ public class GroupHasUsersService {
             groupsByUser.add(groupHasUsers.getGroup());
         }
         return groupsByUser;
+    }
+
+    public List<GroupHasUsers> findAllClients() {
+        List<GroupHasUsers> allClients = new ArrayList<>();
+        for (GroupHasUsers groupHasUsers : groupHasUsersRepository.findAll()) {
+            if (groupHasUsers.getUserRole().equals("Client")) {
+                allClients.add(groupHasUsers);
+            }
+        }
+        return allClients;
     }
 }
