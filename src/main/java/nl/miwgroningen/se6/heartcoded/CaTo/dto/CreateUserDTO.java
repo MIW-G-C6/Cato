284b282672c8980c1c1c0dd1d3d0da380af3cc02
Paul Romkes <p.r.romkes@gmail.com>
2021-11-01 16:15:42 +0100

    DTO's added

---
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dto/CreateUserDTO.java
@@ -0,0 +1,10 @@
+package nl.miwgroningen.se6.heartcoded.CaTo.dto;
+
+/**
+ * @author Paul Romkes <p.r.romkes@gmail.com
+ * <p>
+ * Dit is wat het programma doet
+ */
+
+public class CreateUserDTO {
+}
