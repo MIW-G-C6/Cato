0922feb2bf86795f8f81b646b9c8eab84bb88023
Paul Romkes <p.r.romkes@gmail.com>
2021-11-01 14:27:02 +0100

    Converter directory added

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dto/GroupHasUsersDTO.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dto/GroupHasUsersDTO.java
@@ -1,9 +1,76 @@
-package nl.miwgroningen.se6.heartcoded.CaTo.dto;/**
-* @author Paul Romkes <p.r.romkes@gmail.com
-*
-* Dit is wat het programma doet
-*
-*/
+package nl.miwgroningen.se6.heartcoded.CaTo.dto;
+
+import nl.miwgroningen.se6.heartcoded.CaTo.model.Group;
+import nl.miwgroningen.se6.heartcoded.CaTo.model.User;
+
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ * @author Paul Romkes <p.r.romkes@gmail.com
+ * <p>
+ * Dit is wat het programma doet
+ */
 
 public class GroupHasUsersDTO {
+
+    private static final String[] GROUP_ROLE_OPTIONS = {"Caregiver", "Client"};
+
+    @Id
+    @ManyToOne
+    private Group group;
+
+    @Id
+    @ManyToOne
+    private User user;
+
+    private String userRole;
+
+    private boolean isAdmin;
+
+    public GroupHasUsersDTO(Group group, User user, String userRole, boolean isAdmin) {
+        this.group = group;
+        this.user = user;
+        this.userRole = userRole;
+        this.isAdmin = isAdmin;
+    }
+
+    public GroupHasUsersDTO() {
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public static String[] getGroupRoleOptions() {
+        return GROUP_ROLE_OPTIONS;
+    }
 }
---
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dtoConverter/GroupHasUsersDTOConverter.java
@@ -0,0 +1,10 @@
+package nl.miwgroningen.se6.heartcoded.CaTo.dtoConverter;
+
+/**
+ * @author Paul Romkes <p.r.romkes@gmail.com
+ * <p>
+ * Dit is wat het programma doet
+ */
+
+public class GroupHasUsersDTOConverter {
+}
