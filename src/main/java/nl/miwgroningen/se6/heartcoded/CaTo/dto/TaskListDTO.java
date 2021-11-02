d0cb7cb6091b374c80757735892acb84b8f45268
Shalena Omapersad <shalenao@hotmail.com>
2021-11-02 10:05:45 +0100

    TaskListDTO and TaskListDTOConverter added

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dto/TaskListDTO.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dto/TaskListDTO.java
@@ -1,5 +1,15 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.dto;
 
+import nl.miwgroningen.se6.heartcoded.CaTo.model.Task;
+import nl.miwgroningen.se6.heartcoded.CaTo.model.User;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Paul Romkes <p.r.romkes@gmail.com
  * <p>
@@ -7,4 +17,42 @@ package nl.miwgroningen.se6.heartcoded.CaTo.dto;
  */
 
 public class TaskListDTO {
+
+    private Integer taskListId;
+
+    private List<TaskDTO> taskList;
+
+    private UserDTO client;
+
+    public TaskListDTO(UserDTO client) {
+        this.client = client;
+        this.taskList = new ArrayList<>();
+    }
+
+    public TaskListDTO() {
+    }
+
+    public Integer getTaskListId() {
+        return taskListId;
+    }
+
+    public void setTaskListId(Integer taskListId) {
+        this.taskListId = taskListId;
+    }
+
+    public List<TaskDTO> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<TaskDTO> taskList) {
+        this.taskList = taskList;
+    }
+
+    public UserDTO getClient() {
+        return client;
+    }
+
+    public void setClient(UserDTO client) {
+        this.client = client;
+    }
 }
