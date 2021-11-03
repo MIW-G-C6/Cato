15af1d42e05564b10a65b5cc84b4bc5879fdc30b
Shalena Omapersad <shalenao@hotmail.com>
2021-11-03 13:38:55 +0100

    Added a member mapper

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/service/MemberService.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/service/MemberService.java
@@ -1,5 +1,6 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.service;
 
+import nl.miwgroningen.se6.heartcoded.CaTo.dto.MemberDTO;
 import nl.miwgroningen.se6.heartcoded.CaTo.model.Member;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.MemberRepository;
 import nl.miwgroningen.se6.heartcoded.CaTo.repository.GroupRepository;
@@ -8,6 +9,7 @@ import nl.miwgroningen.se6.heartcoded.CaTo.repository.UserRepository;
 import nl.miwgroningen.se6.heartcoded.CaTo.service.dtoConverter.GroupDTOConverter;
 import nl.miwgroningen.se6.heartcoded.CaTo.service.dtoConverter.GroupHasUsersDTOConverter;
 import nl.miwgroningen.se6.heartcoded.CaTo.service.dtoConverter.UserDTOConverter;
+import nl.miwgroningen.se6.heartcoded.CaTo.service.mappers.MemberMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,33 +27,27 @@ import java.util.Optional;
 @Service
 public class MemberService {
 
-    private final GroupHasUsersDTOConverter groupHasUsersDTOConverter;
-    private final GroupDTOConverter groupDTOConverter;
-    private final UserDTOConverter userDTOConverter;
+    private final MemberMapper memberMapper;
 
     private final GroupRepository groupRepository;
     private final UserRepository userRepository;
     private final MemberRepository memberRepository;
     private final TaskListRepository taskListRepository;
 
-    public MemberService(GroupHasUsersDTOConverter groupHasUsersDTOConverter,
-                         GroupDTOConverter groupDTOConverter,
-                         UserDTOConverter userDTOConverter,
-                         GroupRepository groupRepository,
-                         UserRepository userRepository,
-                         MemberRepository memberRepository,
-                         TaskListRepository taskListRepository) {
-        this.groupHasUsersDTOConverter = groupHasUsersDTOConverter;
-        this.groupDTOConverter = groupDTOConverter;
-        this.userDTOConverter = userDTOConverter;
+    public MemberService(MemberMapper memberMapper, GroupRepository groupRepository, UserRepository userRepository,
+                         MemberRepository memberRepository, TaskListRepository taskListRepository) {
+        this.memberMapper = memberMapper;
         this.groupRepository = groupRepository;
         this.userRepository = userRepository;
         this.memberRepository = memberRepository;
         this.taskListRepository = taskListRepository;
     }
 
-    public void saveMember(GroupHasUsersDTO groupHasUsers) {
-        memberRepository.save(groupHasUsersDTOConverter.toModel(groupHasUsers));
+    public void saveMember(MemberDTO memberDTO) {
+        Member member = memberMapper.toMember(memberDTO);
+        member.setUser(userRepository.getById(memberDTO.getUserId()));
+        //TODO
+        memberRepository.save(member);
     }
 
     public List<GroupHasUsersDTO> getAllByGroupId(Integer groupId) {
