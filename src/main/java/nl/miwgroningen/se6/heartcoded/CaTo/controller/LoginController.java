38553748c877eddefe2bdcec3d185bffa8e9fe82
Paul Romkes <p.r.romkes@gmail.com>
2021-11-03 15:50:55 +0100

    Login edited

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/LoginController.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/LoginController.java
@@ -1,8 +1,6 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.controller;
 
-import nl.miwgroningen.se6.heartcoded.CaTo.dto.UserDTO;
-import nl.miwgroningen.se6.heartcoded.CaTo.model.User;
-import nl.miwgroningen.se6.heartcoded.CaTo.service.UserService;
+import nl.miwgroningen.se6.heartcoded.CaTo.dto.UserLoginDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +16,7 @@ public class LoginController {
 
     @GetMapping("/login")
     protected String showLoginPage(Model model) {
-        model.addAttribute("user", new UserDTO());
+        model.addAttribute("user", new UserLoginDTO());
         return "login";
     }
 }
