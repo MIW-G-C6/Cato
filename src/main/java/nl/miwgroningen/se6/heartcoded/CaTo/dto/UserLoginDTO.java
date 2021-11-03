7980019a04a156b7932efdf522f0c8bf0ef8c5ca
Shalena Omapersad <shalenao@hotmail.com>
2021-11-03 11:38:26 +0100

    Added userLoginDTO

---
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/dto/UserLoginDTO.java
@@ -0,0 +1,32 @@
+package nl.miwgroningen.se6.heartcoded.CaTo.dto;
+
+import javax.persistence.Column;
+
+/**
+ * @author Shalena Omapersad <shalenao@hotmail.com>
+ *
+ * DTO version of model User with password and email.
+ */
+
+public class UserLoginDTO {
+
+    private String password;
+
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
