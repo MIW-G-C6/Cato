73e8d59d3e2feaf112a16856e2ab2cc11fab716d
Erwin Wegter <ewegter@gmail.com>
2021-11-11 14:35:02 +0100

    Removed UserService from SiteAdminDashboardController

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/SiteAdminDashboardController.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/SiteAdminDashboardController.java
@@ -2,9 +2,6 @@ package nl.miwgroningen.se6.heartcoded.CaTo.controller;
 
 import nl.miwgroningen.se6.heartcoded.CaTo.service.MemberService;
 import nl.miwgroningen.se6.heartcoded.CaTo.service.GroupService;
-import nl.miwgroningen.se6.heartcoded.CaTo.service.TaskListService;
-import nl.miwgroningen.se6.heartcoded.CaTo.service.UserService;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,19 +15,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @Controller
 public class SiteAdminDashboardController {
 
-    private UserService userService;
     private GroupService groupService;
     private MemberService memberService;
 
-    public SiteAdminDashboardController(UserService userService, GroupService groupService, MemberService memberService) {
-        this.userService = userService;
+    public SiteAdminDashboardController(GroupService groupService, MemberService memberService) {
         this.groupService = groupService;
         this.memberService = memberService;
     }
 
     @GetMapping("/siteAdminDashboard")
     protected String showSiteAdminDashboard(@ModelAttribute("error") String error, Model model) {
-        model.addAttribute("allUsers", userService.findAllRegisteredUsers());
         model.addAttribute("allClients", memberService.findAllClientsForSiteAdmin());
         model.addAttribute("allGroups", groupService.findAllGroups());
 
