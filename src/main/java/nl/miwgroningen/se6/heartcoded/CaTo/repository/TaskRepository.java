e18c8ece031e181dc5f09b02c45aebc5d5c9d5f6
Shalena Omapersad <shalenao@hotmail.com>
2021-11-17 16:00:09 +0100

    Cleaned up repositories

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/repository/TaskRepository.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/repository/TaskRepository.java
@@ -4,5 +4,6 @@ import nl.miwgroningen.se6.heartcoded.CaTo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
     Integer countAllByAssignedUserNotNull();
 }
