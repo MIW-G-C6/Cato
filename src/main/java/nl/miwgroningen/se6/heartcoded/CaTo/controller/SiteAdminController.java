a931f9b30d0477b61556bacd937885b9397a3e1f
Paul Romkes <p.r.romkes@gmail.com>
2021-11-17 16:05:14 +0100

    breadcrumbs on the site admin pages

--- src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/SiteAdminController.java
+++ src/main/java/nl/miwgroningen/se6/heartcoded/CaTo/controller/SiteAdminController.java
@@ -2,6 +2,7 @@ package nl.miwgroningen.se6.heartcoded.CaTo.controller;
 
 import nl.miwgroningen.se6.heartcoded.CaTo.service.MemberService;
 import nl.miwgroningen.se6.heartcoded.CaTo.service.CircleService;
+import nl.miwgroningen.se6.heartcoded.CaTo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,14 +23,20 @@ public class SiteAdminController {
 
     private CircleService circleService;
     private MemberService memberService;
+    private UserService userService;
 
-    public SiteAdminController(CircleService circleService, MemberService memberService) {
+    public SiteAdminController(CircleService circleService, MemberService memberService, UserService userService) {
         this.circleService = circleService;
         this.memberService = memberService;
+        this.userService = userService;
     }
 
     @GetMapping("/siteAdmin/dashboard")
     protected String showSiteAdminDashboard(@ModelAttribute("error") String error, Model model) {
+        if (isNotSiteAdmin()) {
+            return "redirect:/403";
+        }
+
         model.addAttribute("allClients", memberService.findAllClientsForSiteAdmin());
         model.addAttribute("allCircles", circleService.findAllCircles());
         return "siteAdminDashboard";
@@ -37,13 +44,24 @@ public class SiteAdminController {
 
     @GetMapping("/siteAdmin/userOverview")
     protected String showSiteAdminUserOverview(@ModelAttribute("error") String error) {
+        if (isNotSiteAdmin()) {
+            return "redirect:/403";
+        }
         return "siteAdminUserOverview";
     }
 
     @GetMapping("/siteAdmin/circleClientOverview")
     protected String showSiteAdminCircleClientOverview(@ModelAttribute("error") String error, HttpSession session) {
+        if (isNotSiteAdmin()) {
+            return "redirect:/403";
+        }
 
         session.setAttribute("circleDeleteRedirect", "redirect:/siteAdmin/circleClientOverview");
         return "siteAdminCircleClientOverview";
     }
+
+    private boolean isNotSiteAdmin() {
+        return !userService.currentUserIsSiteAdmin();
+    }
+
 }
