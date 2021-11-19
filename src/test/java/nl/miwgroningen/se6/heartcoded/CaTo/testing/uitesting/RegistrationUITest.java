a94f1adc587ecbf3bc0a729ce063d4f0d01d6fbf
Shalena Omapersad <shalenao@hotmail.com>
2021-11-19 14:44:41 +0100

    Added the linux chrome drivers to the other UI tests

--- src/test/java/nl/miwgroningen/se6/heartcoded/CaTo/testing/uitesting/RegistrationUITest.java
+++ src/test/java/nl/miwgroningen/se6/heartcoded/CaTo/testing/uitesting/RegistrationUITest.java
@@ -1,5 +1,6 @@
 package nl.miwgroningen.se6.heartcoded.CaTo.testing.uitesting;
 
+import org.apache.commons.lang.SystemUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,7 +25,17 @@ public class RegistrationUITest {
 
     @BeforeEach
     void setUp() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        if(SystemUtils.IS_OS_WINDOWS){
+            System.out.println("Selecting Windows Chrome driver");
+            System.setProperty("webdriver.chrome.driver", "chromedrivers\\chromedriver.exe");
+        }
+        else if(SystemUtils.IS_OS_LINUX){
+            System.out.println("Selecting Linux Chrome driver");
+            System.setProperty("webdriver.chrome.driver", "chromedrivers\\chromedriver");
+        }else{
+            throw new UnsupportedOperationException("Operating system not supported by available Chrome web drivers");
+        }
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://localhost:8080/registration");
